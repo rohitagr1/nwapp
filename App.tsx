24791c41e9440be67869f3669af8c7436c0c50cc
Rohit agarwal <rohit32.agarwal@gmail.com>
2024-02-21 00:56:48 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View,Text,SafeAreaView} from 'react-native'
+
+function app(){
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>fuck react !</Text>
+        <Text>fuck react !</Text>
+        <Text>fuck react !</Text>
+        <Text>fuck react !</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default app ;
---
+++ AppPro.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+    View,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    useColorScheme
+} from 'react-native'
+
+function AppPro(){
+const isdarkmode = useColorScheme() === 'dark'
+return(
+    <View style={styles.container}>
+<Text style={isdarkmode ? styles.whitetext : styles.blacktext}>
+    fuck react !
+</Text>
+    </View>
+)
+
+}
+
+const  styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        alignItems : 'center',//horizontal allingment 
+        justifyContent : 'center'//vertical allingmnet
+    },
+    whitetext : {
+        color : '#ffffff'
+    },
+    blacktext : {
+        color : '#000000'
+    }
+})
+
+export default AppPro;
